cf67302f3a0d305b3fb0e0fa9f6126addbd3c703
mazdakfa <mzdk60@gmail.com>
2026-05-14 14:08:36 -0700

    Assign Family Doctor to Patient
    What ? : Assign Family Doctor to Patient
    
    Why ? : to send family doctor id to API.
    
    How ? : create assign family doctor request.(FamilyDoctorRequest)

---
+++ medtrack-backend/src/main/java/com/medtrack/patient/dto/FamilyDoctorRequest.java
@@ -0,0 +1,11 @@
+package com.medtrack.patient.dto;
+
+public class FamilyDoctorRequest {
+    private  Long familyDoctorId;
+
+    public FamilyDoctorRequest() {}
+
+    public Long getFamilyDoctorId() { return familyDoctorId; }
+
+    public void setFamilyDoctorId(Long familyDoctorId) { this.familyDoctorId = familyDoctorId; }
+}
