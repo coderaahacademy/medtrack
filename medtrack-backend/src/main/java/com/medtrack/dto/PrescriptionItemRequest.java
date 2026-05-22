69b87b7f9f7cdfff81433da4e30d1b04d1ced1f3
mazdakfa <mzdk60@gmail.com>
2026-05-22 06:08:36 -0700

    Prescription Management API
    What ? : Prescription Management API
    
    Why ? : to create new Prescription
    
    How ? : update and add new field to Prescription.java
    List<PrescriptionItem> items
    addItems(),getItems(), setItems()
    DTOs : PrescriptionRequest.java, PrescriptionResponse.java, PrescriptionItemRequest.java, PrescriptionItemResponse.java
    Controller : PrescriptionController.java
    Service : PrescriptionService.java
    Repositories : PrescriptionRepository.java, MedicationRepository.java

---
+++ medtrack-backend/src/main/java/com/medtrack/dto/PrescriptionItemRequest.java
@@ -0,0 +1,37 @@
+package com.medtrack.dto;
+
+public class PrescriptionItemRequest {
+
+  private Long medicationId;
+  private String dosage;
+  private String frequency;
+  private Integer durationDays;
+  private Integer quantity;
+  private String instructions;
+
+  public PrescriptionItemRequest() {}
+
+  public Long getMedicationId() { return medicationId; }
+
+  public void setMedicationId(Long medicationId) { this.medicationId = medicationId; }
+
+  public String getDosage() { return dosage; }
+
+  public void setDosage(String dosage) { this.dosage = dosage; }
+
+  public String getFrequency() { return frequency; }
+
+  public void setFrequency(String frequency) { this.frequency = frequency; }
+
+  public Integer getDurationDays() { return durationDays; }
+
+  public void setDurationDays(Integer durationDays) { this.durationDays = durationDays; }
+
+  public Integer getQuantity() { return quantity; }
+
+  public void setQuantity(Integer quantity) { this.quantity = quantity; }
+
+  public String getInstructions() { return instructions; }
+
+  public void setInstructions(String instructions) { this.instructions = instructions; }
+}
---
+++ medtrack-backend/src/main/java/com/medtrack/repository/PrescriptionRepository.java
@@ -0,0 +1,9 @@
+package com.medtrack.repository;
+
+import com.medtrack.entity.Prescription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
+}
