8dee69d98b9de3b90dd3894a501d676be44bee40
mazdakfa <mzdk60@gmail.com>
2026-05-26 03:04:23 -0700

    Fix RequestMapping path
    What ? : Fix RequestMapping path
    
    Why ? : The "/" before the api was missing.
    
    How ? : "/" was placed before the "api" in the Visit and Prescription RequestMapping. "api/" -> "/api/"

--- medtrack-backend/src/main/java/com/medtrack/controller/PrescriptionController.java
+++ medtrack-backend/src/main/java/com/medtrack/controller/PrescriptionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/prescriptions")
+@RequestMapping("/api/prescriptions")
 public class PrescriptionController {
     private final PrescriptionService prescriptionService;
 
--- medtrack-backend/src/main/java/com/medtrack/controller/VisitController.java
+++ medtrack-backend/src/main/java/com/medtrack/controller/VisitController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/visits")
+@RequestMapping("/api/visits")
 public class VisitController {
 
     private final VisitService visitService;
